47b550faac2c2b74a4a047131de9eaf515803fe6
Fredsampaio03 <fredsampaio182@gmail.com>
2023-08-04 19:23:13 -0300

    validador de idade

---
+++ script.test.js
@@ -0,0 +1,18 @@
+/** @jest-environment jsdom */
+
+const { validarIdade } = require("./script")
+
+test ("true", function() {
+
+    var idade = validarIdade  ('18')
+  
+    expect(idade).toBe(true)
+  })
+  
+  test ("false", function() {
+  
+      var idade =  validarIdade ('17')
+    
+      expect(idade).toBe(false)
+    })
+  
